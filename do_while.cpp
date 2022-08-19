bde913c4e2b897421bb00a79938fd9fdd0d197c4
msg2324 <73478662+igiza1213@users.noreply.github.com>
2022-08-19 14:07:53 +0900

    2022/08/19 C 10502
    while/do_while/while_test

---
+++ do_while.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+main(){
+//	int i = 10;
+//	do{
+//		printf("%d ", i);
+//		i--;
+//	}while(i >= 5);
+
+//	int n, i = 1;
+//	printf("정수 입력: ");
+//	scanf("%d", &n);
+//	do{
+//		printf("I love progrmming\n");
+//		i++;
+//	}while(i <= n);
+	
+//	int num;
+//	do{
+//		scanf("%d", &num);
+//		if(num > 0){
+//			if(num % 2 == 1)
+//			{
+//				printf("홀수\n");
+//		 	}else {
+//				printf("짝수\n");	
+//			}
+//		}
+//	}while(num >= 0);
+
+//	int sum = 0, i = 1;
+//	do{
+//		sum += i;
+//		i += 2;
+//	}while(i <= 50);
+//	printf("1부터 50까지 홀수 합: %d", sum);
+
+//	int sum = 0,n;
+//	printf("정수 입력(0이면 종료) : ");
+//	do{
+//		scanf("%d", &n);
+//		sum += n;
+//	}while(n != 0);
+//	printf("합: %d", sum);
+}
---
+++ while.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+main(){
+//	int n = 0;
+//	while(n < 5){
+//		printf("Hi~\n");
+//		n++;
+//	}
+//	printf("n = %d", n);
+	
+//	int i = 1;
+//	while(i <= 10){
+//		printf("#");
+//		i++;
+//	}
+
+//	int num = 1;
+//	while(num <= 5){
+//		printf("반복내용 : %d \n", num);
+//		num++;
+//	}
+//	printf("반복문 종료 후 num = %d \n", num);
+
+//	int i = 30;
+//	while(i >= 10){
+//		printf("%d ", i);
+//		i -= 2;
+//	}
+
+//	int i = 1, sum = 0;
+//	while(i <= 10){
+//		sum += i;
+//		i++;	
+//	}
+//	printf("1부터 10까지의 합 : %d", sum);
+
+//	int i, n, sum = 0, temp= 0;
+//	printf("시작수  : ");
+//	scanf("%d", &i);
+//	printf("마지막 수  : ");
+//	scanf("%d", &n);
+//	temp = i;
+//	while(i <= n){
+//		sum += i;
+//		i++;
+//	}
+//	printf("%d부터 %d까지의 합은 %d입니다\n", temp, n, sum);
+
+//int i = 1;
+//	printf("*** 2단 ***\n");
+//	while(i < 10){
+//		printf("2 * %d = %d\n", i, 2 * i);
+//		i++;
+//	}
+
+//	int dan, i = 9;	
+//	while(true){
+//		i = 9;
+//		printf("몇 단? ");
+//		scanf("%d", &dan);
+//		while(i >= 1){
+//			printf("%d * %d = %d\n", dan, i, dan * i);
+//			i--;
+//		}
+//		if(dan == 0){
+//			printf("bye~");
+//			break;
+//		}
+//	}
+
+//	int count = 1;
+//	while(true){
+//		printf("%d ", count);
+//		count++;
+//		if(count > 10) break;
+//	}
+
+//	int i = 1, sum = 0;
+//	while(true){
+//		sum += i;
+//		i += 2;
+//		if(i > 100) break;
+//	}
+//	printf("1부터 100까지 홀수의 합 = %d\n", sum);
+}
