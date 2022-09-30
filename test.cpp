8b43713649e69e2cbb164f612365c6afbb567de4
igiza1213 <igiza1213@gmail.com>
2022-09-30 13:48:51 +0900

    2022/09/30-semifinal

--- test.cpp
+++ test.cpp
@@ -1,19 +1,43 @@
 #include <stdio.h>
 
+void exam1(){
+
+}
+
+void exam2(){
+
+}
+
+void exam3(){
+
+}
+
+void exam4(){
+
+}
+
+void exam5(){
+
+}
+
+void exam6(){
+
+}
+
+void exam7(){
+
+}
+
+void exam8(){
+
+}
+
+void exam9(){
+
+}
+
 void exam10(){
-	int num, sum = 0;
-	scanf("%d", &num);
-	for (int i = 1;i <= 1000; i++)
-	{
-		sum += i;
-		if (num <= sum)
-		{
-			printf("%d", i);
-			break;
-		}
-		
-	}
-	
+
 }
 
 void exam11(){
@@ -66,6 +90,15 @@ main(){
 		scanf("%d", &num);
 		printf("%d번 문제========================================================================\n", num);
 		switch(num){
+			case 1: exam1(); break;
+			case 2: exam2(); break;
+			case 3: exam3(); break;
+			case 4: exam4(); break;
+			case 5: exam5(); break;
+			case 6: exam6(); break;
+			case 7: exam7(); break;
+			case 8: exam8(); break;
+			case 9: exam9(); break;
 			case 10: exam10(); break;
 			case 11: exam11(); break;
 			case 12: exam12(); break;
